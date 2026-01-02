8d779a19fe348306ddab5867a4ca2adb89d9e3fe
Lucas Galdino <lucas.sva@outlook.com>
2026-01-02 09:33:04 -0300

    style: fix terraform formatting and workflow version

--- infra/live/dev/main.tf
+++ infra/live/dev/main.tf
@@ -13,7 +13,7 @@ module "networking" {
 module "storage" {
   source = "../../modules/storage"
 
-  project_name         = var.project_name
-  environment          = var.environment
-  buckets              = var.buckets
+  project_name = var.project_name
+  environment  = var.environment
+  buckets      = var.buckets
 }
